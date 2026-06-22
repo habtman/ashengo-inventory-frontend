5a3b996afa94e631ff3381cc960b7dcdd2b7f6be
habtman <ashengohabt@email.com>
2026-06-22 13:39:31 +0300

    modified:   src/pages/invoice/InvoiceCreate.jsx

--- src/pages/invoice/InvoiceCreate.jsx
+++ src/pages/invoice/InvoiceCreate.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import invoiceApi from "../../api/invoiceApi";
-import {customerApi} from "../../api/customerApi";
+import customerApi from "../../api/customerApi";
 import { inventoryApi } from "../../api/inventoryApi";
 
 
