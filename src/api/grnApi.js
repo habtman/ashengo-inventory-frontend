a3dd89a127ce9c5771f2cff7d0f42725088827ca
habtman <ashengohabt@email.com>
2026-09-09 17:02:13 +0300

    modified:   src/api/grnApi.js modified:   src/pages/grn/GRNList.jsx

--- src/api/grnApi.js
+++ src/api/grnApi.js
@@ -24,24 +24,6 @@ const grnApi = {
     return apiFetch(`/api/v1/grn?${params.toString()}`);
   },
 
-  getAllForReport: async ({
-    search = "",
-    supplier = "",
-    warehouse = "",
-    dateFrom = "",
-    dateTo = ""
-  } = {}) => {
-    const params = new URLSearchParams();
-
-    if (search) params.append("search", search);
-    if (supplier) params.append("supplier", supplier);
-    if (warehouse) params.append("warehouse", warehouse);
-    if (dateFrom) params.append("dateFrom", dateFrom);
-    if (dateTo) params.append("dateTo", dateTo);
-
-    return apiFetch(`/api/v1/grn/report?${params.toString()}`);
-  },
-
   reverse: (id) =>
     apiFetch(`/api/v1/grn/${id}/reverse`, {
       method: "POST",
