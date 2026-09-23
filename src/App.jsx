c3a4bd4531ac42a33528418e3248d2fa2951883c
habtman <ashengohabt@email.com>
2026-09-23 19:32:26 +0300

    modified:   src/App.jsx renamed:    src/pages/ForgotPassword.jsx -> src/pages/auth/ForgotPassword.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ import InventoryDetails from "./pages/inventory/InventoryDetails";
 import AgingReport from "./pages/customers/reports/AgingReport";
 import CustomerCreditDashboard from "./pages/customers/CustomerCreditDashboard";  
 import ResetPassword from "./pages/auth/ResetPassword"; 
+import ForgotPassword from "./pages/auth/ForgotPassword";  
 
 
 
@@ -90,6 +91,7 @@ export default function App() {
           <Route path="/customers/credit-dashboard" element={<CustomerCreditDashboard />} />  
           <Route path="/inventory/:id" element={<InventoryDetails />} />
           <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />  
 
 
 
